4d034583dfb6e4df73f5594e76c98d97af68b650
Magnewei <Magnewei@Icloud.com>
2024-09-11 02:20:45 +0200

    supabase import and client instantiated

--- src/db/SupabaseService.ts
+++ src/db/SupabaseService.ts
@@ -1,1 +1,5 @@
 import { IDataService, User } from './IDataService';
+import { createClient } from '@supabase/supabase-js';
+import { SUPABASE_URL, SUPABASE_KEY } from '../config/api_keys';
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
